e28ee5a0b91f25f37f955a20c74ddced152368b2
MrSedan <elpashevs@bk.ru>
2019-11-14 18:47:50 +0300

    Login, Register and LogOut

--- newsite/profiles/forms.py
+++ newsite/profiles/forms.py
@@ -1,4 +1,15 @@
 from django import forms
+from django.core.validators import EmailValidator
 
 class LoginForm(forms.Form):
-    login = forms.TextInput()
+    login = forms.CharField(label='Login', max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
+    password = forms.CharField(label='Password', max_length=48, required=True, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    remember = forms.BooleanField(label='Remember Me', required=False)
+    
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(label='First Name', max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
+    second_name = forms.CharField(label='Second Name', max_length=20, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия(необязательно)'}))
+    login = forms.CharField(label='Login', max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
+    email = forms.EmailField(label='Email', max_length=48, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password = forms.CharField(label='Password', max_length=48, required=True, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    re_password = forms.CharField(label='Repeat password', max_length=48, required=True, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль'}))
--- newsite/profiles/urls.py
+++ newsite/profiles/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'profile'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.log_in, name='login'),
+    path('logout/', views.log_out, name='logout'), 
+    path('register/', views.register, name='register')
 ]
