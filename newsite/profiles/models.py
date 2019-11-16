87b7063769baa2216048cf0633de5ade8d8350d3
MrSedan <elpashevs@bk.ru>
2019-11-16 16:35:04 +0300

    Added ids

--- newsite/profiles/models.py
+++ newsite/profiles/models.py
@@ -8,9 +8,9 @@ class Info(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.CharField(name='url', max_length=15)
     about = models.TextField(name='about', max_length=120, blank=False, null=True)
-    
+    readonly_fields = ['id']
     def __str__(self):
-        return self.user.id
+        return f"[id: {self.id}] {self.user.username}"
     
     class Meta:
         verbose_name = 'Информация'
